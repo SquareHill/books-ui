48f561ec890538b51f25a2b7b78fbc977d7337fd
Nick Sjostrom <nick@sjostrom.io>
2024-05-02 14:51:20 -0400

    Testing mantine and open library

---
+++ src/components/search/Search.tsx
@@ -0,0 +1,179 @@
+import { useState } from 'react';
+import {
+  TextInput,
+  Loader,
+  Text,
+  Title,
+  Image,
+  Badge,
+  Skeleton,
+  Avatar,
+  Group,
+} from '@mantine/core';
+import { useDebouncedCallback } from '@mantine/hooks';
+
+type Book = {
+  id: number;
+  title: string;
+  cover_edition_key: string;
+  cover_i: number;
+  author_name: string[];
+  key: string;
+  number_of_pages_median: number;
+  subject: string[];
+  author_key: string[];
+};
+
+type SearchResults = {
+  count: number;
+  items: Book[];
+};
+
+function genCoverImgUrl(coverId: number) {
+  return `https://covers.openlibrary.org/a/id/${coverId}-M.jpg`;
+}
+
+function getSearchResults(query: string): Promise<SearchResults> {
+  return fetch(
+    `https://openlibrary.org/search.json?q=${query}&fields=cover_edition_key,cover_i,author_name,key,number_of_pages_median,title,subject,author_key&limit=10`
+  )
+    .then((response) => response.json())
+    .then((data) => ({
+      count: data.numFound,
+      items: data.docs.map((book: any) => ({
+        id: book.key,
+        title: book.title,
+        cover_i: book.cover_i,
+        author_name: book.author_name,
+        key: book.key,
+        number_of_pages_median: book.number_of_pages_median,
+        subject: book.subject,
+        author_key: book.author_key,
+      })),
+    }));
+}
+
+const Result = ({ book }: { book: Book }) => {
+  const [showImg, setShowImg] = useState(false);
+
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'minmax(auto, 125px) minmax(0, 1fr)',
+        gap: '20px',
+      }}
+    >
+      <Skeleton visible={!showImg}>
+        <Image
+          radius="xs"
+          src={
+            book.cover_i
+              ? genCoverImgUrl(book.cover_i)
+              : `https://placehold.co/125x200?text=${book.title}`
+          }
+          h={'200px'}
+          style={{ maxWidth: '125px', boxShadow: '2px 2px 1px rgba(255,255,255,.2)' }}
+          alt={book.title}
+          onLoad={() => setShowImg(true)}
+          fallbackSrc="https://placehold.co/200x125?text=Placeholder"
+        />
+      </Skeleton>
+      <div>
+        <Title order={3}>{book.title}</Title>
+        <div
+          style={{
+            display: 'flex',
+            gap: '10px',
+            alignItems: 'center',
+            marginTop: '10px',
+            flexWrap: 'flex',
+          }}
+        >
+          {book.author_name.slice(0, 3).map((author, idx) => (
+            <Group gap="sm">
+              <Avatar
+                key={author}
+                size="sm"
+                src={
+                  idx === 0
+                    ? `https://covers.openlibrary.org/a/olid/${book.author_key?.[0]}-S.jpg`
+                    : undefined
+                }
+              />
+              <Text size="xs">{author}</Text>
+            </Group>
+          ))}
+        </div>
+        <div style={{ marginTop: '20px' }}>
+          <Text size="xs">{book.number_of_pages_median} pages</Text>
+          <div style={{ marginTop: '8px' }}>
+            {book.subject && (
+              <div style={{ display: 'flex', gap: '5px', flexWrap: 'wrap' }}>
+                {book.subject.slice(0, 5).map((subject) => (
+                  <Badge key={subject} color="gray">
+                    {subject}
+                  </Badge>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Results = ({ results }: SearchResults) => (
+  <div style={{ display: 'flex', flexDirection: 'column', gap: '30px' }}>
+    {results.items.map((result) => (
+      <Result key={result.key} book={result} />
+    ))}
+  </div>
+);
+
+const Search = () => {
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState<{ id: number; title: string }[]>([]);
+  const [searchResults, setSearchResults] = useState<SearchResults>({ items: [] });
+  const [loading, setLoading] = useState(false);
+
+  const handleSearch = useDebouncedCallback(async (query: string) => {
+    setLoading(true);
+    if (query === '') {
+      setLoading(false);
+      setSearchResults({ items: [] });
+      return;
+    }
+    setSearchResults(await getSearchResults(query));
+    setLoading(false);
+  }, 500);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.currentTarget.value);
+    handleSearch(event.currentTarget.value);
+  };
+
+  return (
+    <>
+      <TextInput
+        value={search}
+        onChange={handleChange}
+        variant="filled"
+        size="xl"
+        placeholder="Search books..."
+        rightSection={loading && <Loader size={20} />}
+      />
+      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        {!!search && !!searchResults?.count && (
+          <Text size={'sm'} color={'rgba(255,255,255,.6)'}>
+            Found {searchResults.count} results
+          </Text>
+        )}
+      </div>
+      {!!search && <Results results={searchResults} />}
+    </>
+  );
+};
+
+export default Search;
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,29 @@
-import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { Title, Text } from '@mantine/core';
+import Search from '@/components/search/Search';
 
 export function HomePage() {
   return (
-    <>
-      <Welcome />
-      <ColorSchemeToggle />
-    </>
+    <div
+      style={{
+        maxWidth: 600,
+        margin: 'auto',
+        display: 'grid',
+        gap: '10px',
+        padding: '50px 0px',
+      }}
+    >
+      <Title>
+        Welcome,{' '}
+        <Text
+          inherit
+          component={'span'}
+          variant="gradient"
+          gradient={{ from: 'indigo', to: 'grape', deg: 28 }}
+        >
+          Book Liker
+        </Text>
+      </Title>
+      <Search />
+    </div>
   );
 }
